d67f0a2b89af250ecf3f81eab9024b2d347caee4
Oleh Fomenko <ofomenko@blockstream.com>
2026-06-17 20:45:44 +0300

    initial, adding polynomial struct

---
+++ src/math/mod.rs
@@ -0,0 +1,63 @@
+use k256::Scalar;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Polynomial {
+    coefficients: Vec<Scalar>,
+}
+
+impl Polynomial {
+    pub fn new(coefficients: Vec<Scalar>) -> Self {
+        Self { coefficients }
+    }
+
+    pub fn eval(&self, x: Scalar) -> Scalar {
+        self.coefficients
+            .iter()
+            .rev()
+            .fold(Scalar::ZERO, |acc, coefficient| acc * x + coefficient)
+    }
+
+    pub fn coeff(&self, i: usize) -> Option<&Scalar> {
+        self.coefficients.get(i)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn scalar(value: u64) -> Scalar {
+        Scalar::from(value)
+    }
+
+    #[test]
+    fn returns_coefficients_by_index() {
+        let polynomial = Polynomial::new(vec![scalar(3), scalar(5), scalar(8)]);
+
+        assert_eq!(polynomial.coeff(0), Some(&scalar(3)));
+        assert_eq!(polynomial.coeff(1), Some(&scalar(5)));
+        assert_eq!(polynomial.coeff(2), Some(&scalar(8)));
+        assert_eq!(polynomial.coeff(3), None);
+    }
+
+    #[test]
+    fn evaluates_empty_polynomial_as_zero() {
+        let polynomial = Polynomial::new(vec![]);
+
+        assert_eq!(polynomial.eval(scalar(7)), Scalar::ZERO);
+    }
+
+    #[test]
+    fn evaluates_constant_polynomial() {
+        let polynomial = Polynomial::new(vec![scalar(42)]);
+
+        assert_eq!(polynomial.eval(scalar(9)), scalar(42));
+    }
+
+    #[test]
+    fn evaluates_polynomial_at_scalar() {
+        let polynomial = Polynomial::new(vec![scalar(3), scalar(2), scalar(5)]);
+
+        assert_eq!(polynomial.eval(scalar(4)), scalar(91));
+    }
+}
