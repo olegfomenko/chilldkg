eaa54506c8cdfeebbd04464d9c14b4e96bcf80d5
Oleh Fomenko <ofomenko@blockstream.com>
2026-06-17 20:57:14 +0300

    adding tagged_hash

---
+++ src/crypto/mod.rs
@@ -0,0 +1,30 @@
+use sha2::{Digest, Sha256};
+
+pub fn tagged_hash(tag: impl AsRef<[u8]>, x: impl AsRef<[u8]>) -> [u8; 32] {
+    let tag_hash = Sha256::digest(tag.as_ref());
+
+    let mut hash = Sha256::new();
+    hash.update(tag_hash);
+    hash.update(tag_hash);
+    hash.update(x.as_ref());
+    hash.finalize().into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn computes_tagged_hash() {
+        let tag = b"chilldkg/test";
+        let x = b"message";
+        let tag_hash = Sha256::digest(tag);
+
+        let mut expected = Sha256::new();
+        expected.update(tag_hash);
+        expected.update(tag_hash);
+        expected.update(x);
+
+        assert_eq!(tagged_hash(tag, x), <[u8; 32]>::from(expected.finalize()));
+    }
+}
